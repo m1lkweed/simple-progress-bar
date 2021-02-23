649dedf24582e1b02a5108ce1ccaa9388555efd5
m1lkweed <m1lkweed@users.noreply.github.com>
2021-02-23 00:33:43 -0500

    Update progress_bar.h
    clean up formatting, add header guards, and prefer fputs to printf

--- progress_bar.h
+++ progress_bar.h
@@ -14,16 +14,15 @@
 //
 //progress_bar returns -1 for invalid locations, -2 for negative bar lengths, -3 for invalid styles, otherwise returns 0
 
-#ifndef PROGRESS_BAR_H
-#define PROGRESS_BAR_H
 #include <stdio.h>
+#include <stdint.h>
 
-int progress_bar(unsigned short x, unsigned short y, long double length, int style, const char* color){
-	const char* s[] = {"\xe2\x96\x88", //shades
+int progress_bar(const uint16_t x, const uint16_t y, long double length, int style, const char *color){
+	const char *s[] = {"\xe2\x96\x88", //shades
 	                   "\xe2\x96\x91",
 	                   "\xe2\x96\x92",
 	                   "\xe2\x96\x93"};
-	const char* h[] = {"\xe2\x96\x88", //slices
+	const char *h[] = {"\xe2\x96\x88", //slices
 	                   "\xe2\x96\x8f",
 	                   "\xe2\x96\x8e",
 	                   "\xe2\x96\x8d",
@@ -31,20 +30,19 @@ int progress_bar(unsigned short x, unsigned short y, long double length, int sty
 	                   "\xe2\x96\x8b",
 	                   "\xe2\x96\x8a",
 	                   "\xe2\x96\x89"};
-	const char* safe_exit = "\x1b[u\x1b[39;49m"; //pop cursor position and reset colors
+	const char *safe_exit = "\x1b[u\x1b[39;49m"; //pop cursor position and reset colors
 	if((x == 0)||(y == 0))return -1; //check valid inputs
 	if(length < 0)return -2;
-	printf("\x1b[s\x1b[%d;%dH%s", x, y, color); //push and move cursor position
+	fprintf(stdout, "\x1b[s\x1b[%d;%dH%s", x, y, color?color:""); //push and move cursor position
 	switch(style % 4){
-	case 0: while(0.5 <= length--)printf("%s",s[0]);break; //no style
-	case 1: while(1 <= length){printf("%s",h[0]);--length;}if(length >= 0.5)printf("%s",h[4]);break; //halves
-	case 2: while(1 <= length){printf("%s",s[0]);--length;}if(length >= 0.25)printf("%s",s[((int)(length*4))%4]);break; //gradient
-	case 3: while(1 <= length){printf("%s",h[0]);--length;}if(length >= 0.125)printf("%s",h[((int)(length*8))%8]);break; //eighths
-	default: printf(safe_exit); return -3;
+	case 0: while(0.5 <= length--)fputs(s[0], stdout);break; //no style
+	case 1: while(1 <= length){fputs(h[0], stdout);--length;}if(length >= 0.5)fputs(h[4], stdout); break; //halves
+	case 2: while(1 <= length){fputs(s[0], stdout);--length;}if(length >= 0.25)fputs(s[((int)(length*4))%4], stdout); break; //gradient
+	case 3: while(1 <= length){fputs(s[0], stdout);--length;}if(length >= 0.125)fputs(h[((int)(length*8))%8], stdout); break; //eighths
+	default: fputs(safe_exit, stdout); return -3;
 	}
-	if(style > 3)printf("\x1b[0K"); //clear line after bar
-	printf(safe_exit);
+	if(style > 3)fputs("\x1b[0K", stdout); //clear line after bar
+	fputs(safe_exit, stdout);
 	fflush(stdout); //prevents visual errors
 	return 0;
 }
-#endif
